c1fc82460bd107de0708acb761b14595c5c1eea8
Yash Puniwala <yashpuniwala@gmail.com>
2024-11-30 10:56:53 +0530

    second commit

--- src/contexts/authContext/index.js
+++ src/contexts/authContext/index.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react"
 import { auth  } from "../../firebase/firebase"
-import {onAuthStateChanged} from "firebase/auth"
+import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = React.createContext()
 
